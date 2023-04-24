dd86d15c33b3029a486732a99ff1ce52f9a6ee1c
김백민 <silverbullet087@gmail.com>
2023-04-24 18:45:52 +0900

    chore: Set up initial project structure and sample code

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+import { FileImageOutlined, HomeOutlined } from "@ant-design/icons";
+import Viewer from "./Viewer";
+
+const { Header, Content, Sider } = Layout;
+
+function App() {
+  const [selectedMenuItem, setSelectedMenuItem] = useState("home");
+
+  const handleMenuItemClick = (menuItem) => {
+    setSelectedMenuItem(menuItem.key);
+  };
+
+  const isHomeSelected = selectedMenuItem === "home";
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider breakpoint="lg" collapsedWidth="0">
+        <div style={{ height: "32px", margin: "16px" }} />
+        <Menu theme="dark" mode="inline" selectedKeys={[selectedMenuItem]} onClick={handleMenuItemClick}>
+          <Menu.Item key="home" icon={<HomeOutlined />}>
+            Home
+          </Menu.Item>
+          <Menu.Item key="viewer" icon={<FileImageOutlined />}>
+            Image Viewer
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header style={{ background: "#fff", padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div style={{ padding: 24, minHeight: 360 }}>
+            {isHomeSelected && <h1>Welcome to the Home Page!</h1>}
+            {!isHomeSelected && <Viewer />}
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default App;
